b5077c02c6208fe90b6e40192aa4214b2f690d5e
dragoni7 <sdgibson18@outlook.com>
2024-06-14 23:22:53 -0700

    Switched to session storage

--- src/services/LocalStorageService.js
+++ src/services/LocalStorageService.js
@@ -1,13 +1,13 @@
 export default {
     get: function(key) {
-      return Promise.resolve(JSON.parse(localStorage.getItem(key)));
+      return Promise.resolve(JSON.parse(window.sessionStorage.getItem(key)));
     },
     set: function(key, data) {
-      localStorage.setItem(key, JSON.stringify(data));
+      window.sessionStorage.setItem(key, JSON.stringify(data));
       return Promise.resolve(data);
     },
     delete: function(key) {
-      localStorage.removeItem(key);
+      window.sessionStorage.removeItem(key);
       return Promise.resolve('Deleted');
     },
   };
