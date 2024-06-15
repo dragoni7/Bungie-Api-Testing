a670c0ff2aa2a093b5eb0095fc04c62d43d98095
dragoni7 <sdgibson18@outlook.com>
2024-06-15 15:45:16 -0700

    refactor OAuth attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
 import './App.css';
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import BungieApiComponent from './components/BungieApiComponent'
-import Reducer from "./Reducer"
 
-class App extends Component {
-  render() {
+function App() {
+
+  useEffect(() => {
+    window.location.replace(`https://www.bungie.net/en/OAuth/Authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code`)
+
+    var tokenData = undefined;
+    var authCode = undefined;
+
+    if (window.location.href.includes("code=")) {
+      fetch('https://www.bungie.net/Platform/App/OAuth/Token/', {
+        method: 'POST',
+        headers: {
+          'X-API-Key': process.env.REACT_APP_API_KEY,
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Basic ${window.btoa(`${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`)}`
+        },
+        body: new URLSearchParams({
+          'client_id': process.env.REACT_APP_CLIENT_ID,
+          'grant_type': "authorization_code",
+          'code': authCode
+        }).toString()
+      }).then(function(response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function(data) {
+        tokenData = data;
+        console.log(data);
+      })
+    }
+
+  }, [])
+
+
     return (
       <div className="App">
-        <Reducer apiKey={process.env.REACT_APP_API_KEY}>
-          {({ store, actions }) => (
-              <div>
-                {store.authenticated ? (
-                  <div>
-                    <BungieApiComponent />
-                  </div>
-                ) : (
-                  <button onClick={actions.onAuthorize}>Log in</button>
-                )}
-              </div>
-          )}
-        </Reducer>
+        
       </div>
     );
-  }
 }
 
 export default App;
